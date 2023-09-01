25a6e0abf39b0d0485294edb667bab07db5c1987
Leucisticboi <116482248+Leucisticboi@users.noreply.github.com>
2023-09-01 04:00:14 -0600

    Created all necessary javascript files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Define your routes
+const apiRouter = require('./routes/index');
+app.use('/api', apiRouter);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
